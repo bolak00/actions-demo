ad5876e33216253e84391c2fe6468f2e962096ca
Kaan K. Bolat <k.bolat@belastingdienst.nl>
2024-11-24 18:30:56 +0100

    Fix!!

---
+++ src/main/java/com/kaan/jakarta/hello/HealthResource.java
@@ -0,0 +1,18 @@
+package com.kaan.jakarta.hello;
+
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+
+@Path("health")
+public class HealthResource {
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response checkHealth() {
+        return Response.ok()
+                .build();
+    }
+}
